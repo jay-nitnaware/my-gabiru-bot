21201f9d18a6599ef902daeab002ebde1f8bc485
Jay <jaynvnit@gmail.com>
2025-08-29 19:13:57 +0530

    implement initial server setup with Express and Telegram bot integration

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+const axios = require("axios");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/new-message", async (req, res) => {
+    const {message} = req.body;
+
+    if(!message || !message.text || !message.chat || !message.chat.id) {
+        return res.sendStatus(400);
+    }
+
+    if(message.text.toLowerCase().indexOfs("gabiru") < 0) {
+        return res.sendStatus(200);
+    }
+
+    const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "7783323718:AAEdlJIjN3yBKvSF3jGmWQu2OsHRnL1-J2Y";
+
+    axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+        chat_id: message.chat.id,
+        text: "Meow, Meow !!",
+    })
+    .then(() => {
+        console.log("Message sent");
+        res.end("ok");
+    })
+    .catch((err) => {
+        console.error("Error :", err);
+        res.end("Error :" + err);
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
