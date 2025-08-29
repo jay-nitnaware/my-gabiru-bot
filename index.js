90c87d34f8970240f2f199e91379cd279468e221
Jay <jaynvnit@gmail.com>
2025-08-29 19:14:51 +0530

    refactor code formatting for improved readability in index.js

--- index.js
+++ index.js
@@ -7,32 +7,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/new-message", async (req, res) => {
-    const {message} = req.body;
+  const { message } = req.body;
 
-    if(!message || !message.text || !message.chat || !message.chat.id) {
-        return res.sendStatus(400);
-    }
+  if (!message || !message.text || !message.chat || !message.chat.id) {
+    return res.sendStatus(400);
+  }
 
-    if(message.text.toLowerCase().indexOfs("gabiru") < 0) {
-        return res.sendStatus(200);
-    }
+  if (message.text.toLowerCase().indexOfs("gabiru") < 0) {
+    return res.sendStatus(200);
+  }
 
-    const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "7783323718:AAEdlJIjN3yBKvSF3jGmWQu2OsHRnL1-J2Y";
+  const TELEGRAM_BOT_TOKEN =
+    process.env.TELEGRAM_BOT_TOKEN ||
+    "7783323718:AAEdlJIjN3yBKvSF3jGmWQu2OsHRnL1-J2Y";
 
-    axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
-        chat_id: message.chat.id,
-        text: "Meow, Meow !!",
+  axios
+    .post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+      chat_id: message.chat.id,
+      text: "Meow, Meow !!",
     })
     .then(() => {
-        console.log("Message sent");
-        res.end("ok");
+      console.log("Message sent");
+      res.end("ok");
     })
     .catch((err) => {
-        console.error("Error :", err);
-        res.end("Error :" + err);
+      console.error("Error :", err);
+      res.end("Error :" + err);
     })
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
