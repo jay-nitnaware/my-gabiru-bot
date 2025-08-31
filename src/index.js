249a01a4aeaea69145594d65576be008c947aee3
Jay <jaynvnit@gmail.com>
2025-08-31 11:35:13 +0530

    refactor index.js to use createApp for Express setup and remove Telegram bot integration

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-const axios = require("axios");
+import {createApp} from "./createApp,js"
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = createApp();
 
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const PORT = process.env.PORT || 3000;
 
-app.post("/new-message", async (req, res) => {
-  
-  const { message } = req.body;
-
-  console.log("Webhook payload:", req.body);
-
-  if (!message || !message.text || !message.chat || !message.chat.id) {
-    return res.sendStatus(200);
-  }
-
-  try {
-    await axios.post(
-      `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
-      {
-        chat_id: message.chat.id,
-        text: "Meow, Meow !!",
-      }
-    );
-    res.end("ok");
-  } catch (err) {
-    console.error("Error :", err);
-    res.end("Error :" + err);
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
